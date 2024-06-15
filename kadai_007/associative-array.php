667b03ec744c89c5ba373053273a0ecd01b65791
wataaiai1009 <169005311+wataaiai1009@users.noreply.github.com>
2024-06-16 06:51:26 +0900

    Add files via upload

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
 
+<body>
+  <?php
+  $Array = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+  print_r($Array);
+  ?>
+</body>
+</html>
