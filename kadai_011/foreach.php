f81edfaca84eb69167d39f75c2c69cc9ae568dae
wataaiai1009 <169005311+wataaiai1009@users.noreply.github.com>
2024-06-19 09:50:29 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_011</title>
+ </head>
+
+ <body>
+  <?php
+  $data = ['名前' => '玉ねぎ', '値段' => '200', '産地' => '北海道'];
+  
+  foreach ($data as $key => $value) {
+    echo "{$key} : {$value}" . '<br>';
+  }
+  ?>
+ </body>
+
+ </html>
